704354dc7205ad15d3c5fe43e9adbc57ec14897c
Zeki Sherif <zeki.sherif@gmail.com>
2014-11-25 01:36:39 -0500

    Added some dao methods to finance

---
+++ MIS/src/java/ViewModel/DoctorMonitorVM.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ViewModel;
+
+import Model.ProceduresModel;
+import Model.VisitationRecordsModel;
+import java.util.List;
+
+/**
+ *
+ * @author TheKey
+ */
+public class DoctorMonitorVM 
+{
+    List<VisitationRecordsModel> VisitRecords;
+    List<ProceduresModel> ProcedureList;
+}
