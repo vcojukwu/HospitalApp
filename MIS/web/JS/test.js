182a91dd4f7d36189af5606f024adb021034f13e
vcojukwu <victor.ojukwu94@gmail.com>
2014-11-24 22:40:14 -0500

    Some UI for doc search in financial view

--- MIS/web/JS/test.js
+++ MIS/web/JS/test.js
@@ -143,3 +143,15 @@ function addRow2(r){
    							<i class="fa fa-times-circle"></i>\
 					  </button>';
 }
+
+function isChecked(){
+	var check = document.getElementById("docbyID");
+	if(check.checked){
+		document.getElementById("docID").style.display = "initial";
+		document.getElementById("doc").style.display = "none";
+	}
+	else {
+		document.getElementById("docID").style.display = "none";
+		document.getElementById("doc").style.display = "initial";
+	}
+}
